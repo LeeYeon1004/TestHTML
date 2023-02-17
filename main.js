2e47da8c27a026d1aafc15e0a76b11228e6934b1
LoiBH <loi.buihuu@vti.com.vn>
2023-02-17 17:35:36 +0700

    approval

---
+++ main.js
@@ -0,0 +1,21 @@
+let value = "";
+function handleGetValue(name) {
+  value = name;
+}
+const listOptions = [
+  {
+    name: "option 1",
+  },
+  {
+    name: "option 2",
+  },
+  {
+    name: "option 3",
+  },
+  {
+    name: "option 4",
+  },
+  {
+    name: "option 5",
+  },
+];
